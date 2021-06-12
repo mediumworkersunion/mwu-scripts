04b191fe75cab626268f6a8b9a6f7d4c5500b9ed
Daniel Nelson <daniel@dcn.io>
2021-06-11 14:19:13 -1000

    fix dir detection when sourced from .zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-## determine script dir and whether sourced or run. perl because osx and linux compatibility is hard
-base_dir=$(perl -e 'use File::Basename; use Cwd "abs_path"; print dirname(abs_path(@ARGV[0]));' -- "${BASH_SOURCE[0]}")
+## Env setup
 
-# sanity check if we found the right dir 
+# determine script dir. Perl because osx and linux compatibility is hard. Weid shell var for .zshrc support
+base_dir=$(perl -e 'use File::Basename; use Cwd "abs_path"; print dirname(abs_path(@ARGV[0]));' -- "${BASH_SOURCE[0]:-${(%):-%x}}")
+
+# sanity check if we found the right dir
 if ! [ -x "${base_dir}/bootstrap.sh" -a -d "${base_dir}/scripts" -a -d "${base_dir}/bootstrap.d" ]; then
-  echo "FATAL: Could not find all required files. Ask for help"
+  echo "FATAL: Could not find all required files. Ask for help and say: "
+  echo "base_dir was detected as $base_dir"
   #return if we're sourced, exit if not
   return 1 2>/dev/null || exit 1
 fi
