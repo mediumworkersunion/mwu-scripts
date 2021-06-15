4b75577efe66792ca0df9604bd497321648c4398
Daniel Nelson <daniel@dcn.io>
2021-06-15 13:06:22 -1000

    Allow other default branches than just main

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -10,13 +10,35 @@ function cur_branch() {
   git branch --show-current
 }
 
-function main_branch() {
-  echo "main"
+function is_git_tree() {
+  [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == "true" ]] || fatal "Must be run inside a git work tree"
 }
-function error() {
-  echo -e "${RED}ERROR: ${PLAIN}${@}"
+
+function main_branch() {
+  is_git_tree
+  #attempt to get default branch from git config
+  stored_branch="$(git config --get mwu.defaultbranch)"
+  if [[ ! $? || $stored_branch == "" ]]; then
+    stored_branch=$(get_default_branch)
+    git config mwu.defaultbranch "${stored_branch}"
+  fi
+  echo "${stored_branch}"
+}
+
+function get_default_branch() {
+  # make sure we're inside a git repo and have the gh cli
+  is_git_tree
+  command -v gh >/dev/null || fatal "gh cli is required"
+  gh repo view --json 'defaultBranchRef' --template '{{.defaultBranchRef.name}}'
+}
+  
+function fatal() {
+  echo -e "⛔️ ${@}" 2>&1
   exit 1
 }
+function error() {
+  echo -e "⛔️ ${@}" 2>&1
+}
 function warn() {
-  echo -e "${YELLOW}WARN: ${PLAIN}${@}"
+  echo -e "⚠️  ${@}" 2>&1
 }
